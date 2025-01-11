71fea1bd854ad231d465bf181514ef0bd47d51f5
Pablo Cazorla <miscuentas2pablo@gmail.com>
2025-01-11 11:03:35 -0300

    add base

---
+++ src/game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor(canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    this.ctx = this.canvas.getContext("2d");
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+  }
+}
+
+export default Game;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import Game from "./game";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const Martes = new Game("martes");
+});
